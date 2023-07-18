25e9161be4704dab10a82cbccfbd10de45605c0f
rosmildoLopes <[200~123316625+rosmildoLopes@users.noreply.github.com~>
2023-07-17 23:19:31 -0300

    hero section finished

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,16 @@
+"use client"
+
+
 import React from 'react'
 import Image from 'next/image'
 import CustomButton from './CustomButton'
 
 
 const Hero = () => {
+  const handleScroll = () => {
+    // Add your logic here
+  };
+
   return (
     <div className='hero'>
       <div className='flex-1 pt-36 padding-x'>
@@ -11,12 +18,24 @@ const Hero = () => {
           Find, book, or rent a car -- quickly and easily!
         </h1>
 
-        <p className='hero__subtitle'>Streamline your rental experience with our effortless booking process.</p>
+        <p className='hero__subtitle'>
+          Streamline your rental experience with our effortless booking process.
+        </p>
 
-        <CustomButton />
+        <CustomButton
+          title='Explore Cars'
+          containerStyles='bg-primary-blue text-white rounded-full mt-10'
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className='hero__image-container'>
+        <div className='hero__image'>
+          <Image src='/hero.jpg' alt='hero' fill className='object-contain' />
+          <div className='hero__image-overlay' />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Hero
